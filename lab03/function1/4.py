b1de11af90a90e32588feffd9f5e983e77b8e47b
mosk4t <moskatmalades@gmail.com>
2024-02-05 16:57:05 +0600

    upd

---
+++ lab03/function1/4.py
@@ -0,0 +1,14 @@
+def isPrime(a):
+    if a<2:
+        return false
+    for i in range(2, int(a**0.5) + 1):
+        if a % i == 0:
+            return False
+    return True
+
+nums = input().split()
+prime_list = []
+for i in nums():
+	if isPrime(int(i)):
+		prime_list.append(i)
+print(prime_list)
